41da61b706da8e78e0d437c80ac38294dfaa63b7
Philson Nah <philsonnah@msn.com>
2021-09-10 13:36:25 +0800

    Solved bugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Login from "./pages/login";
 
-function App()
-{
-  return <Login />;
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
